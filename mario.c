ee108d39b4bfb1bcc526fcfee5a10ecbedd8d246
Ballaschk <ballaschk@users.noreply.github.com>
2018-02-12 00:19:13 +0000

    20180212T001755Z

---
+++ mario.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        printf("Number is between 0 and 23\n");
+        n = get_int("Number: ");
+
+    }
+    while (n < 0 || n > 23);
+// Print out this many rows
+    for (int i = 0; i < n ; i++)
+    {
+
+// Print out this many spaces
+        for (int m = 0; m < n - i - 1; m++)
+        {
+            printf("%s", " ");
+        }
+// Print out this many hashes
+        for (int j = 0; j < i + 2; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
